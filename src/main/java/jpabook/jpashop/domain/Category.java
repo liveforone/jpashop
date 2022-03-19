0000d137c3a8e4e61a0a2c5c73c66eab5704060b
liveforone <yc4852@gmail.com>
2022-03-19 14:53:39 +0900

    Good commit

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany  //중간테이블 매핑, DB는 다대다 매핑이 불가능해서
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),  //양방향을 다 명시 해줘서 매핑
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")  //셀프 연관관계 매핑
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
